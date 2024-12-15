6cbb062d0cfeb00856e6168dd49695e81fb90f67
Anastas Kostov <anastas.kostov@tbs.tech>
2024-12-15 14:25:16 +0200

    refactoring

--- dotnet/adventofcode/Program.cs
+++ dotnet/adventofcode/Program.cs
@@ -1,14 +1,3 @@
 ﻿using adventofcode.builder;
 
 var adventofcode = new AdventOfCodeBuilder();
-
-adventofcode
-    .Select(
-        selector => selector.HistorianHysteria,
-        selector => selector.RedNosedReports,
-        selector => selector.MullItOver,
-        selector => selector.CeresSearch,
-        selector => selector.PrintQueue,
-        selector => selector.RestroomRedoubt
-    )
-    .Run();
