c0d10b694ba081a747cb6bee4981c9cbc5ab37f3
Anastas Kostov <anastas.kostov@tbs.tech>
2024-12-26 23:05:57 +0200

    day 24 dotnet part one

--- dotnet/adventofcode/solutions/day-24/CrossedWires.cs
+++ dotnet/adventofcode/solutions/day-24/CrossedWires.cs
@@ -1,4 +1,6 @@
-﻿namespace adventofcode;
+﻿using System.Runtime.CompilerServices;
+
+namespace adventofcode;
 
 public class CrossedWires : ExecutionMeasure, ISolution
 {
@@ -15,8 +17,12 @@ public class CrossedWires : ExecutionMeasure, ISolution
         return ((resultPartOne.ToString(), millisecondsPartOne), (resultPartTwo.ToString(), millisecondsPartTwo));
     }
 
-    private Dictionary<string, int> Wires = [];
-    private Queue<string> Instructions = [];
+    private Dictionary<string, byte> Wires = [];
+    private SortedList<int, string> Z_Wires = [];
+    private List<string> Instructions = [];
+    private Graph Graph = new();
+
+    private static readonly char[] separator = [' ', '-', '>'];
 
     public void SetInput(string inputSource)
     {
@@ -27,21 +33,90 @@ public class CrossedWires : ExecutionMeasure, ISolution
         Wires = lines
             .Take(separatorIndex)
             .Select(line => line.Split(':', StringSplitOptions.TrimEntries))
-            .ToDictionary(parts => parts[0], parts => int.Parse(parts[1]));
+            .ToDictionary(parts => parts[0], parts => byte.Parse(parts[1]));
 
-        Instructions = new Queue<string>(
+        Instructions = new List<string>(
             lines.Skip(separatorIndex + 1)
                 .Where(line => !string.IsNullOrEmpty(line))
         );
     }
 
-    private int SolutionPartOne()
+    private void BuildGraph()
     {
-        throw new NotImplementedException();
+        for (int idx = 0; idx < Instructions.Count; idx++)
+        {
+            var instruction = Instructions[idx]
+                .Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            if (instruction[3].StartsWith('z'))
+            {
+                int key = int.Parse(instruction[3][1..]);
+                Z_Wires.Add(key, instruction[3]);
+            }
+
+            Graph.AddNode(
+                value: instruction[3],
+                leftNodeValue: instruction[0],
+                rightNodeValue: instruction[2],
+                bitwiseOperation: instruction[1]);
+        }
     }
 
-    private int SolutionPartTwo()
+    private long BinaryToInt(List<byte> bits)
     {
-        throw new NotImplementedException();
+        long result = 0;
+        for (int idx = bits.Count - 1; idx >= 0; idx--)
+        {
+            result = (result << 1) | bits[idx];
+        }
+        return result;
+    }
+
+    private long SolutionPartOne()
+    {
+        if (Graph == null || Graph.Count == 0)
+        {
+            BuildGraph();
+        }
+
+        var bits = new List<byte>();
+        for (int idx = 0; idx < Z_Wires.Count; idx++)
+        {
+            byte value = WireValue(Graph.GetNode(Z_Wires[idx]));
+            bits.Add(value);
+        }
+
+        long result = BinaryToInt(bits);
+        return result;
+
+        byte WireValue(Node node)
+        {
+            if (!Wires.TryGetValue(node.Left.Value, out var left))
+            {
+                left = WireValue(node.Left);
+            }
+
+            if (!Wires.TryGetValue(node.Right.Value, out var right))
+            {
+                right = WireValue(node.Right);
+            }
+
+            return node.BitwiseOperation switch
+            {
+                "AND" => (byte)(left & right),
+                "OR" => (byte)(left | right),
+                "XOR" => (byte)(left ^ right)
+            };
+        }
+    }
+
+    private string SolutionPartTwo()
+    {
+        if (Graph == null || Graph.Count == 0)
+        {
+            BuildGraph();
+        }
+
+        return "";
     }
 }
---
+++ dotnet/adventofcode/solutions/day-24/Graph.cs
@@ -0,0 +1,57 @@
+﻿namespace adventofcode;
+
+public class Node()
+{
+    public string Value { get; set; }
+    public Node Left { get; set; }
+    public Node Right { get; set; }
+    public string BitwiseOperation { get; set; }
+}
+
+public class Graph
+{
+    public int Count { get; set; }
+    public Dictionary<string, Node> Nodes { get; set; }
+
+    public Graph()
+    {
+        Nodes = [];
+    }
+
+    public void AddNode(string value, string leftNodeValue, string rightNodeValue, string bitwiseOperation)
+    {
+        if (!Nodes.TryGetValue(value, out var node))
+        {
+            node = new Node()
+            {
+                Value = value
+            };
+        }
+
+        node.Left = this.GetOrCreateNode(leftNodeValue);
+        node.Right = this.GetOrCreateNode(rightNodeValue);
+        node.BitwiseOperation = bitwiseOperation;
+        Nodes[value] = node;
+
+        Count++;
+    }
+
+    public Node GetNode(string value)
+    {
+        return Nodes.TryGetValue(value, out var node) ? node : null;
+    }
+
+    private Node GetOrCreateNode(string value)
+    {
+        var node = this.GetNode(value);
+        if (node == null)
+        {
+            node = new Node() { Value = value };
+            Nodes[value] = node;
+
+            Count++;
+        }
+
+        return node;
+    }
+}
